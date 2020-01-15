a84973d511901dfb8d28e347e9a5edd96dec7398
erikrios <erikriosetiawan49@gmail.com>
2020-01-15 21:51:36 +0700

    Integrated data binding on activity

--- app/src/main/java/com/erikriosetiawan/globofly/WelcomeActivity.kt
+++ app/src/main/java/com/erikriosetiawan/globofly/WelcomeActivity.kt
@@ -1,12 +1,16 @@
 package com.erikriosetiawan.globofly
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.erikriosetiawan.globofly.databinding.ActivityWelcomeBinding
 
 class WelcomeActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityWelcomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_welcome)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_welcome)
     }
 }
