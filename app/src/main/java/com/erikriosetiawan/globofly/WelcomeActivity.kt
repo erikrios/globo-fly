cc96bf141f57a508f13d1cb058dd7a898521587e
erikrios <erikriosetiawan49@gmail.com>
2020-01-15 21:58:06 +0700

    Add setOnClickListener to the button

--- app/src/main/java/com/erikriosetiawan/globofly/WelcomeActivity.kt
+++ app/src/main/java/com/erikriosetiawan/globofly/WelcomeActivity.kt
@@ -1,16 +1,30 @@
 package com.erikriosetiawan.globofly
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.erikriosetiawan.globofly.databinding.ActivityWelcomeBinding
 
-class WelcomeActivity : AppCompatActivity() {
+class WelcomeActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityWelcomeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_welcome)
+
+        // To be replaced by retrofit code
+        binding.message.text = getString(R.string.message)
+        binding.button.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            binding.button.id -> {
+                Toast.makeText(this, "Button clicked!", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
