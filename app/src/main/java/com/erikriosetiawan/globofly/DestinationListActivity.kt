61386c2f60b71870d441234a4eabaccb0ae322da
erikrios <erikriosetiawan49@gmail.com>
2020-01-16 20:36:39 +0700

    Add destination list activity

---
+++ app/src/main/java/com/erikriosetiawan/globofly/DestinationListActivity.kt
@@ -0,0 +1,12 @@
+package com.erikriosetiawan.globofly
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DestinationListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_destination_list)
+    }
+}
