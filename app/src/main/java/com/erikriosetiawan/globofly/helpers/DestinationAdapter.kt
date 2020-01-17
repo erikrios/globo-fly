b1510c03e90c0833353b08534d62122dc887999b
erikrios <erikriosetiawan49@gmail.com>
2020-01-17 09:48:33 +0700

    Add the adapter code

--- app/src/main/java/com/erikriosetiawan/globofly/helpers/DestinationAdapter.kt
+++ app/src/main/java/com/erikriosetiawan/globofly/helpers/DestinationAdapter.kt
@@ -1,2 +1,38 @@
 package com.erikriosetiawan.globofly.helpers
 
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.erikriosetiawan.globofly.databinding.ListItemBinding
+import com.erikriosetiawan.globofly.models.Destination
+
+class DestinationAdapter(
+    private val context: Context,
+    private val destinations: List<Destination>
+) : RecyclerView.Adapter<DestinationAdapter.ViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val layoutInflater = LayoutInflater.from(context)
+        val itemBinding = ListItemBinding.inflate(layoutInflater, parent, false)
+        return ViewHolder(itemBinding)
+    }
+
+    override fun getItemCount(): Int = destinations.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bindViews(destinations[position]) {
+            // Intent code here
+        }
+    }
+
+    inner class ViewHolder(private val binding: ListItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bindViews(destination: Destination, listener: (Destination) -> Unit) {
+            binding.destination = destination
+            binding.root.setOnClickListener { listener(destination) }
+        }
+    }
+}
