4104afc8645cfd0ad1114c2c975366223415c8e9
erikrios <erikriosetiawan49@gmail.com>
2020-01-18 10:49:37 +0700

    Add destination detail activity

---
+++ app/src/main/java/com/erikriosetiawan/globofly/activities/DestinationDetailActivity.kt
@@ -0,0 +1,13 @@
+package com.erikriosetiawan.globofly.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.erikriosetiawan.globofly.R
+
+class DestinationDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_destination_detail)
+    }
+}
