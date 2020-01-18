d0d2ae99256636692ef06581be1770e22ed8f7bb
erikrios <erikriosetiawan49@gmail.com>
2020-01-18 11:19:45 +0700

    Integrated the data binding

--- app/src/main/java/com/erikriosetiawan/globofly/activities/DestinationDetailActivity.kt
+++ app/src/main/java/com/erikriosetiawan/globofly/activities/DestinationDetailActivity.kt
@@ -2,12 +2,16 @@ package com.erikriosetiawan.globofly.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.erikriosetiawan.globofly.R
+import com.erikriosetiawan.globofly.databinding.ActivityDestinationDetailBinding
 
 class DestinationDetailActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityDestinationDetailBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_destination_detail)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_destination_detail)
     }
 }
