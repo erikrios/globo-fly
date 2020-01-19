6332cc36265fef9250a65d5782d525d333ab0882
erikrios <erikriosetiawan49@gmail.com>
2020-01-19 15:15:40 +0700

    Add destination create activity

---
+++ app/src/main/java/com/erikriosetiawan/globofly/activities/DestinationCreateActivity.kt
@@ -0,0 +1,13 @@
+package com.erikriosetiawan.globofly.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.erikriosetiawan.globofly.R
+
+class DestinationCreateActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_destination_create)
+    }
+}
