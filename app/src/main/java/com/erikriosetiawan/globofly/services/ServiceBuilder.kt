c50ee0c84080318b11641897afe9730a5d01e3ef
erikrios <erikriosetiawan49@gmail.com>
2020-01-21 19:01:39 +0700

    Create OkHttpClient

--- app/src/main/java/com/erikriosetiawan/globofly/services/ServiceBuilder.kt
+++ app/src/main/java/com/erikriosetiawan/globofly/services/ServiceBuilder.kt
@@ -1,8 +1,15 @@
 package com.erikriosetiawan.globofly.services
 
+import okhttp3.OkHttpClient
+
 object ServiceBuilder {
 
 
     // Before release, change this URL to your live server URL such as "https://smartherd.com/"
     private const val URL = "http://10.0.2.2:9000/"
+
+    // Create OkHttp Client
+    private val okHttp = OkHttpClient.Builder()
+
+
 }
