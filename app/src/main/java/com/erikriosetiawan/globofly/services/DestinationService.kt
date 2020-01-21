91413fd4585aeaaa4a41014e3394126ef68f46aa
erikrios <erikriosetiawan49@gmail.com>
2020-01-21 18:54:37 +0700

    Add the getDestinationList() function into the interface

--- app/src/main/java/com/erikriosetiawan/globofly/services/DestinationService.kt
+++ app/src/main/java/com/erikriosetiawan/globofly/services/DestinationService.kt
@@ -1,5 +1,11 @@
 package com.erikriosetiawan.globofly.services
 
+import com.erikriosetiawan.globofly.models.Destination
+import retrofit2.Call
+import retrofit2.http.GET
+
 interface DestinationService {
 
+    @GET("destination")
+    fun getDestinationList(): Call<List<Destination>>
 }
