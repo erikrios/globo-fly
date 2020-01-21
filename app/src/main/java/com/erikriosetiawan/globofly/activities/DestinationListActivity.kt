63b27e7ebe8c3b8497e75a788ff07304793bb12c
erikrios <erikriosetiawan49@gmail.com>
2020-01-21 19:25:42 +0700

    Make Http GET request and receive response using Retrofit

--- app/src/main/java/com/erikriosetiawan/globofly/activities/DestinationListActivity.kt
+++ app/src/main/java/com/erikriosetiawan/globofly/activities/DestinationListActivity.kt
@@ -8,6 +8,12 @@ import com.erikriosetiawan.globofly.R
 import com.erikriosetiawan.globofly.databinding.ActivityDestinationListBinding
 import com.erikriosetiawan.globofly.helpers.DestinationAdapter
 import com.erikriosetiawan.globofly.helpers.SampleData
+import com.erikriosetiawan.globofly.models.Destination
+import com.erikriosetiawan.globofly.services.DestinationService
+import com.erikriosetiawan.globofly.services.ServiceBuilder
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class DestinationListActivity : AppCompatActivity() {
 
@@ -35,7 +41,28 @@ class DestinationListActivity : AppCompatActivity() {
     private fun loadDestinations() {
 
         // To be replaced by retrofit code
-        binding.destinyRecyclerView.adapter =
-            DestinationAdapter(binding.root.context, SampleData.DESTINATIONS)
+//        binding.destinyRecyclerView.adapter =
+//            DestinationAdapter(binding.root.context, SampleData.DESTINATIONS)
+        val destinationService = ServiceBuilder.buildService(DestinationService::class.java)
+
+        val requestCall = destinationService.getDestinationList()
+
+        requestCall.enqueue(object : Callback<List<Destination>> {
+
+            override fun onResponse(
+                call: Call<List<Destination>>,
+                response: Response<List<Destination>>
+            ) {
+                if (response.isSuccessful) {
+                    val destinationList = response.body()
+                    binding.destinyRecyclerView.adapter =
+                        destinationList?.let { DestinationAdapter(binding.root.context, it) }
+                }
+            }
+
+            override fun onFailure(call: Call<List<Destination>>, t: Throwable) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+        })
     }
 }
