2e1912518c9c8682ac507f36adae03071c4be60f
erikrios <erikriosetiawan49@gmail.com>
2020-01-21 18:58:12 +0700

    Add base url

--- app/src/main/java/com/erikriosetiawan/globofly/services/ServiceBuilder.kt
+++ app/src/main/java/com/erikriosetiawan/globofly/services/ServiceBuilder.kt
@@ -2,4 +2,7 @@ package com.erikriosetiawan.globofly.services
 
 object ServiceBuilder {
 
+
+    // Before release, change this URL to your live server URL such as "https://smartherd.com/"
+    private const val URL = "http://10.0.2.2:9000/"
 }
