907b045a810d81bef0492dcd23c47aeef12205c4
erikrios <erikriosetiawan49@gmail.com>
2020-01-24 08:19:47 +0700

    Using path parameters to fetch data

--- app/src/main/java/com/erikriosetiawan/globofly/services/DestinationService.kt
+++ app/src/main/java/com/erikriosetiawan/globofly/services/DestinationService.kt
@@ -3,9 +3,13 @@ package com.erikriosetiawan.globofly.services
 import com.erikriosetiawan.globofly.models.Destination
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface DestinationService {
 
     @GET("destination")
     fun getDestinationList(): Call<List<Destination>>
+
+    @GET("destination/{id}")
+    fun getDestination(@Path("id") id: Int): Call<Destination>
 }
