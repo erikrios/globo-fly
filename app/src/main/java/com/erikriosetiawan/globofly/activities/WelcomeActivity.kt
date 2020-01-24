4d3e74a802d0e0f5ca247776b195a968276561f4
erikrios <erikriosetiawan49@gmail.com>
2020-01-24 19:40:32 +0700

    Retrieve data from different server

--- app/src/main/java/com/erikriosetiawan/globofly/activities/WelcomeActivity.kt
+++ app/src/main/java/com/erikriosetiawan/globofly/activities/WelcomeActivity.kt
@@ -3,10 +3,16 @@ package com.erikriosetiawan.globofly.activities
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.erikriosetiawan.globofly.R
 import com.erikriosetiawan.globofly.databinding.ActivityWelcomeBinding
+import com.erikriosetiawan.globofly.services.MessageService
+import com.erikriosetiawan.globofly.services.ServiceBuilder
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class WelcomeActivity : AppCompatActivity(), View.OnClickListener {
 
@@ -20,7 +26,32 @@ class WelcomeActivity : AppCompatActivity(), View.OnClickListener {
         )
 
         // To be replaced by retrofit code
-        binding.message.text = getString(R.string.message)
+//        binding.message.text = getString(R.string.message)
+
+        val messageService = ServiceBuilder.buildService(MessageService::class.java)
+        val requestCall = messageService.getMessages("http://10.0.2.2:7000/messages")
+
+        requestCall.enqueue(object : Callback<String> {
+
+            override fun onResponse(call: Call<String>, response: Response<String>) {
+                if (response.isSuccessful) {
+                    val msg = response.body()
+                    binding.message.text = msg
+                } else {
+                    Toast.makeText(
+                        this@WelcomeActivity,
+                        "Failed to retrieve items",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+
+            override fun onFailure(call: Call<String>, t: Throwable) {
+                Toast.makeText(this@WelcomeActivity, "Failed to retrieve items", Toast.LENGTH_LONG)
+                    .show()
+            }
+        })
+
         binding.button.setOnClickListener(this)
     }
 
