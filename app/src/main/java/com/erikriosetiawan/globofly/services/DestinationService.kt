4a0df7f44bac5d3ebd93ab364aab310304aa587f
erikrios <erikriosetiawan49@gmail.com>
2020-01-24 19:04:25 +0700

    Using Query Map to send multiple query parameters

--- app/src/main/java/com/erikriosetiawan/globofly/services/DestinationService.kt
+++ app/src/main/java/com/erikriosetiawan/globofly/services/DestinationService.kt
@@ -4,12 +4,12 @@ import com.erikriosetiawan.globofly.models.Destination
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
-import retrofit2.http.Query
+import retrofit2.http.QueryMap
 
 interface DestinationService {
 
     @GET("destination")
-    fun getDestinationList(@Query("country") country: String?, @Query("count") count: String?): Call<List<Destination>>
+    fun getDestinationList(@QueryMap filter: HashMap<String, String>): Call<List<Destination>>
 
     @GET("destination/{id}")
     fun getDestination(@Path("id") id: Int): Call<Destination>
